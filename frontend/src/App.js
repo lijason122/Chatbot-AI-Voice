2ca4b3a5fe02a77d985f9270494ffd9ad4370930
JASON-LI-LAPTOP\jsnli <jason.li@tklabs.com>
2025-05-28 22:36:35 -0400

    Update the payload to send messages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -79,7 +79,7 @@ const sendMessage = async () => {
     const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/chat`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ message: history }),
+      body: JSON.stringify({ messages: history }),
     });
     const data = await res.json();
     const reply = { role: "assistant", text: data.response };
